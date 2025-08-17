52e11f42de7d37612045dcdaab7877d145f16d7b
neilrocks <swapnilroy22@gmail.com>
2025-08-17 14:16:01 +0530

    Room creation logic added

---
+++ src/handlers/roomHandler.ts
@@ -0,0 +1,16 @@
+import {Socket} from "socket.io";
+import {v4 as UUIDv4} from "uuid";
+const roomHandler = (socket: Socket) => {
+    const createRoom = () => {
+        const roomId = UUIDv4(); // this will be our unique id for the room
+        socket.join(roomId); // we will make socket connection join our room
+        socket.emit("roomCreated", {roomId}); // emit the roomId to the client
+        console.log(`Room created with ID: ${roomId}`);
+    };
+    const joinRoom = ({roomId}:{roomId:string}) => {
+        console.log("New user has joined room ",roomId);
+    };
+    socket.on("createRoom", createRoom);
+    socket.on("joinRoom", joinRoom);
+};
+export default roomHandler;
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
+// Node js server will act as my SFU and relay the webrtc packets to other client
+import cors from "cors";
 import express from "express";
 import http from "http";
 import {Server} from "socket.io";
 import serverConfig from "./config/serverConfig.js";
-import cors from "cors";
+import roomHandler from "./handlers/roomHandler.js";
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: "*",
-        methods:["GET","POST"]
-    }
+        methods: ["GET", "POST"],
+    },
 });
-io.on("connection",(socket)=>{
+io.on("connection", (socket) => {
     console.log("New user connected");
-    socket.on("disconnect",()=>{
+    roomHandler(socket);
+    socket.on("disconnect", () => {
         console.log("User disconnected");
     });
 });
